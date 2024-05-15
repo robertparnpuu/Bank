13b5e43c2bb86a35a7dbd0e8ba9e02172ed1c004
Robert Pärnpuu <robert.parnpuu@gmail.com>
2024-05-15 19:28:20 +0300

    Initial commit

---
+++ src/test/java/com/rparnp/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rparnp.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
