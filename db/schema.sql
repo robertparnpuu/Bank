12a431f00f8c6a45b8b1cc34719c2fb54fdf4170
Robert Pärnpuu <robert.parnpuu@gmail.com>
2024-05-16 15:49:38 +0300

    Add database schema generation

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE bank;
+
+\c bank
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS account (
+    account_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    customer_id UUID NOT NULL
+);
+
+CREATE TYPE currency_type AS ENUM ('EUR', 'SEK', 'GBP', 'USD');
+
+CREATE TABLE IF NOT EXISTS balance (
+    account_id UUID NOT NULL,
+    amount NUMERIC DEFAULT 0.0 NOT NULL,
+    currency currency_type NOT NULL,
+    PRIMARY KEY (account_id, currency),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+CREATE TYPE direction_type AS ENUM ('IN', 'OUT');
+
+CREATE TABLE IF NOT EXISTS transaction (
+    transaction_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    account_id UUID NOT NULL,
+    amount NUMERIC NOT NULL,
+    currency currency_type NOT NULL,
+    direction direction_type NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
